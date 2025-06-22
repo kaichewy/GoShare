450bd150e7336571f2fdb732e52ba5bd17ca8a65
Kai Chew <kai.cyk10@gmail.com>
2025-06-22 18:29:31 +0700

    Set up api folder in frontend

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,12 @@
+import { API_URL } from "@/config";
+
+
+export const login = async (email, password) => {
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({ email, password }),
+  });
+  return res.json();
+};
