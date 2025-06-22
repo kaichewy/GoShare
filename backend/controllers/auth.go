95799b76270b211480ae127f7838f93ff7efd069
Kai Chew <kai.cyk10@gmail.com>
2025-06-22 16:19:10 +0700

    Changed login function to temporarily use unhashed passwords

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kaichewy/GoShare/backend/db"
 	"github.com/kaichewy/GoShare/backend/models"
-	"github.com/kaichewy/GoShare/backend/utils"
+	// "github.com/kaichewy/GoShare/backend/utils"
 )
 
 func Login(c *gin.Context) {
@@ -36,7 +36,19 @@ func Login(c *gin.Context) {
 	attempted_password := req.Password
 	correct_password := user_info.Password
 
-	if (utils.CheckPasswordHash(attempted_password, correct_password)) {
+	// if (utils.CheckPasswordHash(attempted_password, correct_password)) {
+	// 	log.Println("logged in successfully")
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "Login successful",
+	// 	})
+	// } else {
+	// 	c.JSON(http.StatusUnauthorized, gin.H{
+	// 		"error": "Invalid password",
+	// 	})
+	// }
+	
+	// temporarily use un-hashed password
+	if (attempted_password == correct_password) {
 		log.Println("logged in successfully")
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Login successful",
